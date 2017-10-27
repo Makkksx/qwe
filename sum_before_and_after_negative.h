dd00603f616dd598ced249a6edc3ff164a70af87
Makkksx <m.vologdin@mail.ru>
2017-10-27 20:43:56 +0300

    Update sum_before_and_after_negative.h

--- sum_before_and_after_negative.h
+++ sum_before_and_after_negative.h
@@ -1,5 +1,5 @@
 #ifndef  __SUM_BEFORE_AND_AFTER_NEGATIVE_H__
 #define __SUM_BEFORE_AND_AFTER_NEGATIVE_H__
-int sum_before_and_after_negative(int mas[], int ssize, int sum);
+int sum_before_and_after_negative(int mas[], int ssize);
 #endif
 
