735dce2540e377fe7364d45ab730264b82c21136
Makkksx <m.vologdin@mail.ru>
2017-11-17 18:30:32 +0300

    Lr3

--- index_first_negative.h
+++
@@ -1,6 +0,0 @@
-#ifndef  __INDEX_FIRST_NEGATIVE_H__
-#define __INDEX_FIRST_NEGATIVE_H__
-
-
-int index_first_negative(int mas[], int ssize);
-#endif 
--- sum_between_negative.h
+++
@@ -1,4 +0,0 @@
-#ifndef __SUM_BEFORE_AND_AFTER_NEGATIVE_H__
-#define __SUM_BEFORE_AND_AFTER_NEGATIVE_H__
-int sum_between_negative(int mas[], int ssize);
-#endif
